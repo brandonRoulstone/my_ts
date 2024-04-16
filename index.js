d0f7f8b95eb75e05bcab5be9124ef23f970ef4d0
Brandon <brandonroulstone71@gmail.com>
2024-04-16 16:22:56 +0200

    Typescript

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var duplicateArr = [1, 1, 11, 1, 2, 2, 2, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8, 9, 10, 10];
+function checkduplicates(arr) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (newArr.indexOf(arr[i]) === -1) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+console.log(checkduplicates(duplicateArr));
+function sort(arrOfNums) {
+    if (typeof Array) {
+        return arrOfNums.sort(function (a, b) { return a - b; });
+    }
+}
+var av = [6969, 2343, 12121, 67, 77744, 4744, 1, 2, 34, 5, 6, 6, 7, 7, 8, 9, 9, 10, 30, 38, 43, 500];
+console.log(sort(av));
+function compare(password, prev) {
+    // let hashedPwd: string = 'Brandon'
+    if (!password.localeCompare(prev)) {
+        console.log('match');
+    }
+    else {
+        console.log('no match');
+    }
+}
+console.log(compare('brndon', 'brndon'));
---
+++ index.ts
@@ -0,0 +1,38 @@
+let duplicateArr: Array<number> = [1,1,11,1,2,2,2,4,4,4,4,4,4,5,6,7,8,9,10,10]; 
+
+function checkduplicates(arr :Array<number>){
+    let newArr:Array<number> = [];
+    for(let i:number = 0; i < arr.length; i++){
+        if(newArr.indexOf(arr[i]) === -1){
+            newArr.push(arr[i]);
+        }
+    }
+
+    return newArr;
+}
+
+console.log(checkduplicates(duplicateArr));
+
+function sort(arrOfNums:Array<number>){
+    if(typeof Array<number>){
+        return arrOfNums.sort((a:number, b:number)=> a-b)
+    }
+}
+
+let av: Array<number> = [6969, 2343, 12121, 67, 77744, 4744, 1, 2, 34, 5, 6, 6, 7, 7, 8, 9, 9, 10, 30, 38, 43 , 500]
+
+console.log(sort(av));
+
+function compare(password: string, prev: string){
+    // let hashedPwd: string = 'Brandon'
+    if(!password.localeCompare(prev)){
+        console.log('match');
+    } else {
+        console.log('no match');
+    }
+}
+
+console.log(compare('brndon', 'brndon'));
+
+
+export {}
